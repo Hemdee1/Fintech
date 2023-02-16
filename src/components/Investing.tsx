66c3e1639b72a0cc934591760e2a29dacb7feed7
hemdee <sanusi5110@gmail.com>
2023-02-16 15:39:11 +0100

    added investing section

---
+++ src/components/Investing.tsx
@@ -0,0 +1,24 @@
+import { CircleBubble } from "./icons";
+
+const Investing = () => {
+  return (
+    <section className="w-fullscreen mx-auto max-w-full px-4 sm:px-16 xl:px-[136px] pt-12 h-[653px] relative overflow-hidden">
+      <span className="absolute inset-0 scale-[2] sm:scale-110">
+        <CircleBubble />
+      </span>
+      <div className="w-[562px] max-w-full">
+        <h2 className="text-2xl font-bold bg-gradient-to-r from-[#D9DCF1] to-[#A5AFF2] bg-clip-text text-transparent inline-block">
+          Investing with Calone
+        </h2>
+        <h5 className="text-secondary mt-4 text-base leading-7 w-full">
+          At Calone, we believe that everyone should have access to smart,
+          effortless savings. That's why we've created a savings platform that
+          makes it easy for you to grow your wealth, without sacrificing your
+          lifestyle.
+        </h5>
+      </div>
+    </section>
+  );
+};
+
+export default Investing;
