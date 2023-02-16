acba237df508449f9d14d9baf51a2453671d98dd
hemdee <sanusi5110@gmail.com>
2023-02-16 15:40:38 +0100

    data updated

---
+++ src/data.ts
@@ -0,0 +1,46 @@
+import { DriverIcon, GiftIcon, ShieldIcon } from "./components/icons";
+
+export const priority = [
+  {
+    title: "Budget Tracking",
+    desc: "Top of your finances with budget tracking feature. Our app will help you keep track of your spending and savings, so you can make informed decisions about your finances.",
+    Icon: DriverIcon,
+  },
+  {
+    title: "Secure and Insured",
+    desc: "Feel confident about your savings with Calone finance. Our digital platform is fully secure and insured, so your money is always safe and protected.",
+    Icon: ShieldIcon,
+  },
+  {
+    title: "Goals and Rewards",
+    desc: "Stay motivated to save with Calone goals and rewards system. Set your savings goals, track your progress, and earn rewards for reaching your milestones.",
+    Icon: GiftIcon,
+  },
+];
+
+export const offers = [
+  {
+    title: "Round-Up Savings",
+    desc: "Make saving a habit with our round-up savings feature. We'll round up your purchases to the nearest dollar and automatically transfer the change to your savings account.",
+  },
+  {
+    title: "24/7 Access",
+    desc: "Take control of your savings with 24/7 access. Check your account balance, track your progress, and manage your savings anytime, anywhere with our user-friendly app and website.",
+  },
+  {
+    title: "High Interest Rates",
+    desc: "Earn more on your savings with Calone high interest rates. Our rates are competitive and transparent, with no hidden fees.",
+  },
+  {
+    title: "Automated Savings",
+    desc: "Never miss a chance to save with our automated savings feature. Simply set your savings goals and watch your wealth grow.",
+  },
+  {
+    title: "Mobile Deposits",
+    desc: "Deposit your money from anywhere, anytime with mobile deposit feature. Simply take a photo of your check with your smartphone and deposit it directly into your savings account.",
+  },
+  {
+    title: "High Interest Rates",
+    desc: "Earn more on your savings with Calone high interest rates. Our rates are competitive and transparent, with no hidden fees.",
+  },
+];
