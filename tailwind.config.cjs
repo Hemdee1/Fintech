6547e0d58f21a0191257efabbbe73d89d657cf09
hemdee <sanusi5110@gmail.com>
2023-02-16 15:38:31 +0100

    animation added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,29 @@ module.exports = {
     extend: {
       fontFamily: {
         Satoshi: "Satoshi, sans-serif",
+        Zendot: "Zen Dots, cursive",
       },
       width: {
         fullscreen: "1512px",
+      },
+      colors: {
+        primary: "#000210",
+        secondary: "#777A98",
+      },
+      keyframes: {
+        moveRight: {
+          "0%": { left: "-40px" },
+          "20%": {
+            left: "300px",
+          },
+          "100%": {
+            left: "300px",
+          },
+        },
+      },
+      animation: {
+        moveRight: "moveRight 3s linear infinite",
+        moveRightDelay: "moveRight 3s 0.5s linear infinite",
       },
     },
   },
